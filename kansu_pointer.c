919e478c84ca9bca833047b440e9deb0e0393a3f
WataruSuzuki42 <65997901+WataruSuzuki42@users.noreply.github.com>
2020-07-11 13:48:55 +0900

    Create kansu_pointer.c
    C:/ogisVSC/tools/msys64/mingw64/bin/../lib/gcc/x86_64-w64-mingw32/9.1.0/../../../../x86_64-w64-mingw32/lib/../lib/libmingw32.a(lib64_libmingw32_a-crt0_c.o): In function `main':
    E:/mingwbuild/mingw-w64-crt-git/src/mingw-w64/mingw-w64-crt/crt/crt0_c.c:18: undefined reference to `WinMain'
    collect2.exe: error: ld returned 1 exit status
    ターミナルの処理が終了しました (終了コード: 1)

---
+++ kansu_pointer.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+
+static void test1();
+static void test2();
+static void test3();
+static void test4();
+static void test5();
+
+static int test_num = 0;
+
+void (*const All_test[])() = {
+    &test1,
+    &test2,
+    &test3,
+    &test4,
+    &test5,
+};
+
+static int main(void) {
+    All_test[test_num]();
+    return 0;
+}
+
+static void test1(){
+    printf("1\n");
+    test_num++;
+}
+
+static void test2(){
+    printf("1\n");
+    test_num++;
+}
+
+static void test3(){
+    printf("1\n");
+    test_num++;
+}
+
+static void test4(){
+    printf("1\n");
+    test_num++;
+}
+
+static void test5(){
+    printf("1\n");
+    test_num++;
+}
